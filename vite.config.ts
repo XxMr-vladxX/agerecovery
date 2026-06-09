b2215ab3cf0138918c74a50e15d253a95f30ba1d
XxMr-vladxX <Vladimir.e.felix@outlook.es>
2026-06-09 01:13:07 -0700

    Añadiendo configuración de despliegue automático

--- vite.config.ts
+++ vite.config.ts
@@ -3,34 +3,20 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-
-function figmaAssetResolver() {
-  return {
-    name: 'figma-asset-resolver',
-    resolveId(id) {
-      if (id.startsWith('figma:asset/')) {
-        const filename = id.replace('figma:asset/', '')
-        return path.resolve(__dirname, 'src/assets', filename)
-      }
-    },
-  }
-}
-
 export default defineConfig({
   plugins: [
-    figmaAssetResolver(),
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
   resolve: {
     alias: {
-      // Alias @ to the src directory
+      // @ apunta a la carpeta 'src'
       '@': path.resolve(__dirname, './src'),
     },
   },
-
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
+  // Configuración para manejar archivos estáticos
   assetsInclude: ['**/*.svg', '**/*.csv'],
+  
+  // base: './' asegura que las rutas sean relativas en el servidor (crucial para Nginx)
+  base: './', 
 })
